f03d8f49d55fb89854d5e25edab3c0e2abf635a4
ozan <56032671+ozansh@users.noreply.github.com>
2024-01-27 22:55:31 +0300

    started to learn go, added hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ozansh/go-url-shortener
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello Wordl")
+
+	fmt.Println("The tine is: ", time.Now())
+}
